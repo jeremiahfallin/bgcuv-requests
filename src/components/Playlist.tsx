890e91660ca0c89aa8ae3301758ea4a522b2dc64
Jeremiah <jeremiah.fallin@gmail.com>
2022-07-13 06:57:05 -0700

    second commit

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -4,7 +4,7 @@ import {
   Image,
   Text,
   Grid,
-  Button,
+  ScrollArea,
   Group,
   useMantineTheme,
 } from '@mantine/core';
@@ -22,42 +22,44 @@ const SongList: React.FC<{
   return (
     <Container>
       <h2>Requests:</h2>
-      <Grid style={{ flexDirection: 'column' }}>
-        {items?.length &&
-          items
-            .filter((item: any) => !item.explicit)
-            .map((item: any) => {
-              item = item.track;
-              return (
-                <Grid.Col
-                  md={12}
-                  lg={12}
-                  style={{ maxWidth: 340 }}
-                  key={item.id}
-                >
-                  <Card shadow="sm" p="lg">
-                    <Group
-                      position="left"
-                      style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-                    >
-                      <Card.Section>
-                        <Image
-                          src={item.album.images?.[2].url}
-                          alt={item.album.name}
-                          height={64}
-                          width={64}
-                        />
-                      </Card.Section>
+      <ScrollArea style={{ height: 600 }} offsetScrollbars type="auto">
+        <Grid style={{ flexDirection: 'column' }}>
+          {items?.length &&
+            items
+              .filter((item: any) => !item.explicit)
+              .map((item: any) => {
+                item = item.track;
+                return (
+                  <Grid.Col
+                    md={12}
+                    lg={12}
+                    style={{ maxWidth: 340 }}
+                    key={item.id}
+                  >
+                    <Card shadow="sm" p="lg">
+                      <Group
+                        position="left"
+                        style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+                      >
+                        <Card.Section>
+                          <Image
+                            src={item.album.images?.[2].url}
+                            alt={item.album.name}
+                            height={64}
+                            width={64}
+                          />
+                        </Card.Section>
 
-                      <Text weight={500}>{item.artists[0].name}</Text>
-                    </Group>
-                    <Text>Song: {item.name}</Text>
-                    <Text>Album: {item.album.name}</Text>
-                  </Card>
-                </Grid.Col>
-              );
-            })}
-      </Grid>
+                        <Text weight={500}>{item.artists[0].name}</Text>
+                      </Group>
+                      <Text>Song: {item.name}</Text>
+                      <Text>Album: {item.album.name}</Text>
+                    </Card>
+                  </Grid.Col>
+                );
+              })}
+        </Grid>
+      </ScrollArea>
     </Container>
   );
 };
--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -9,6 +9,7 @@ import {
   Group,
   useMantineTheme,
   LoadingOverlay,
+  ScrollArea,
 } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import { trpc } from '~/utils/trpc';
@@ -44,61 +45,62 @@ const SongList: React.FC<{
   return (
     <Container>
       <h2>Results:</h2>
-
-      <Grid style={{ flexDirection: 'column' }}>
-        {items?.length &&
-          items
-            .filter((item: any) => !item.explicit)
-            .map((item: any) => {
-              return (
-                <Grid.Col
-                  md={12}
-                  lg={12}
-                  style={{ maxWidth: 340 }}
-                  key={item.id}
-                >
-                  <Card shadow="sm" p="lg" style={{ position: 'relative' }}>
-                    <Group
-                      position="left"
-                      style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-                    >
-                      <LoadingOverlay
-                        visible={addTrackToPlaylistMutation.isLoading}
-                      />
-                      <Card.Section>
-                        <Image
-                          src={item.album.images?.[2].url}
-                          alt={item.album.name}
-                          height={64}
-                          width={64}
+      <ScrollArea style={{ height: 600 }} offsetScrollbars type="auto">
+        <Grid style={{ flexDirection: 'column' }}>
+          {items?.length &&
+            items
+              .filter((item: any) => !item.explicit)
+              .map((item: any) => {
+                return (
+                  <Grid.Col
+                    md={12}
+                    lg={12}
+                    style={{ maxWidth: 340 }}
+                    key={item.id}
+                  >
+                    <Card shadow="sm" p="lg" style={{ position: 'relative' }}>
+                      <Group
+                        position="left"
+                        style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+                      >
+                        <LoadingOverlay
+                          visible={addTrackToPlaylistMutation.isLoading}
                         />
-                      </Card.Section>
+                        <Card.Section>
+                          <Image
+                            src={item.album.images?.[2].url}
+                            alt={item.album.name}
+                            height={64}
+                            width={64}
+                          />
+                        </Card.Section>
 
-                      <Text weight={500}>{item.artists[0].name}</Text>
-                    </Group>
-                    <Text>Song: {item.name}</Text>
-                    <Text>Album: {item.album.name}</Text>
+                        <Text weight={500}>{item.artists[0].name}</Text>
+                      </Group>
+                      <Text>Song: {item.name}</Text>
+                      <Text>Album: {item.album.name}</Text>
 
-                    <Button
-                      variant="light"
-                      color="blue"
-                      fullWidth
-                      style={{ marginTop: 14 }}
-                      onClick={async () => {
-                        addTrackToPlaylistMutation.mutate({
-                          playlistId,
-                          trackId: item.id,
-                          code: secretCode,
-                        });
-                      }}
-                    >
-                      Request
-                    </Button>
-                  </Card>
-                </Grid.Col>
-              );
-            })}
-      </Grid>
+                      <Button
+                        variant="light"
+                        color="blue"
+                        fullWidth
+                        style={{ marginTop: 14 }}
+                        onClick={async () => {
+                          addTrackToPlaylistMutation.mutate({
+                            playlistId,
+                            trackId: item.id,
+                            code: secretCode,
+                          });
+                        }}
+                      >
+                        Request
+                      </Button>
+                    </Card>
+                  </Grid.Col>
+                );
+              })}
+        </Grid>
+      </ScrollArea>
     </Container>
   );
 };
